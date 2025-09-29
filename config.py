f865fc58c2097aefacbbb671dce889068fcceea6
Egemestry87 <udarcevegor87@gmail.com>
2025-09-29 19:40:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Bot configuration - REPLACE WITH YOUR ACTUAL BOT TOKEN
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7484223754:AAHjJea-BYA57AO-_2OLCB-SEBanyAYL5UM')
+
+# If you don't want to use .env file, replace the line above with:
+# BOT_TOKEN = "your_actual_bot_token_here"
